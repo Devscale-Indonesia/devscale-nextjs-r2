2d075d14557a358781ecac0b9a8b65a2bf8955d3
rezarinaldi <rez4rinaldi@gmail.com>
2025-03-09 14:32:19 +0700

    chore: update next.config.mjs for setting r2 public url

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "pub-...", // `R2_PUBLIC_URL`
+        pathname: "/**",
+        protocol: "https",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
